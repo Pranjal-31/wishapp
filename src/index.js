f41f6f0bb79295773535b45aa2dea8f4fccf9910
Pranjal <pranjals3108@gmail.com>
2021-08-27 02:57:14 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BsClockFill } from "react-icons/bs";
+
+let name ="pranjal";
+
+let greeting ="";
+
+const hr= new Date().getHours();
+const min= new Date().getMinutes();    
+const dt= new Date().toLocaleDateString();
+
+
+
+if(hr>=4 && hr<12) {
+  greeting = "Morning";
+}
+
+else if(hr>=12 && hr<16) {
+  greeting ="Afternoon";
+}
+
+else if(hr>=16 && hr<20) {
+  greeting="Evening";
+}
+
+else {
+  greeting ="Night";
+}
+
+ReactDOM.render(
+  <div className="img">
+    <h1 style={{fontSize:30}} > Good {greeting} {name}. </h1><br></br>
+    <div className="time-wrap">
+    {hr}:{min}
+    <br/>
+    <BsClockFill/>
+    </div>
+    
+    <h3 > {dt} </h3>
+  </div>,
+ document.getElementById('root')
+);
+
